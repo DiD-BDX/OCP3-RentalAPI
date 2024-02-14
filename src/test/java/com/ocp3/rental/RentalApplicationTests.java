4055349ac6f898e7b71be1b2ccde3b0a1d027121
Didier Barriere Doleac <didierdoleac@hotmail.com>
2024-02-14 17:38:51 +0100

    installation projet spring-boot

---
+++ src/test/java/com/ocp3/rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocp3.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
