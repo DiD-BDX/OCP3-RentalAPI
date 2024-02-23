20326d75444fdd8cd706de86c0ce257a38608cdd
Didier Barriere Doleac <didierdoleac@hotmail.com>
2024-02-23 21:25:08 +0100

    Refactorisation de la méthode "me" dans ResourceController a l'aide d'une methode getUserFromRequest

--- src/main/java/com/ocp3/rental/controller/ResourceController.java
+++ src/main/java/com/ocp3/rental/controller/ResourceController.java
@@ -27,23 +27,26 @@ public class ResourceController {
 
     @GetMapping("/me")
     public ResponseEntity<?> me(HttpServletRequest request) {
-    // Récupère le token du header de la requête
-    String token = request.getHeader("Authorization").substring(7); // Supprime "Bearer "
+        // Récupère l'utilisateur à partir de la requête
+        USERS user = getUserFromRequest(request);
 
-    // Récupère l'email de l'utilisateur à partir du token
-    String email = jwtService.getUserEmailFromToken(token);
+        // Crée une map avec les informations de l'utilisateur
+        Map<String, Object> userData = new LinkedHashMap<>();
+        userData.put("name", user.getName());
+        userData.put("email", user.getEmail());
+        userData.put("created_at", user.getCreated_at());
+        userData.put("updated_at", user.getUpdated_at());
 
-    // Récupère l'utilisateur à partir de l'email
-    USERS user = userRepository.findByEmail(email);
+        // Retourne les informations de l'utilisateur sous format JSON
+        return ResponseEntity.ok(userData);
+    }
 
-    // Crée une map avec les informations de l'utilisateur
-    Map<String, Object> userData = new LinkedHashMap<>();
-    userData.put("name", user.getName());
-    userData.put("email", user.getEmail());
-    userData.put("created_at", user.getCreated_at());
-    userData.put("updated_at", user.getUpdated_at());
-
-    // Retourne les informations de l'utilisateur sous format JSON
-    return ResponseEntity.ok(userData);
-}
+    private USERS getUserFromRequest(HttpServletRequest request) {
+        // Récupère le token du header de la requête
+        String token = request.getHeader("Authorization").substring(7); // Supprime "Bearer "
+        // Récupère l'email de l'utilisateur à partir du token
+        String email = jwtService.getUserEmailFromToken(token);
+        // Récupère l'utilisateur à partir de l'email
+        return userRepository.findByEmail(email);
+    }
 }
